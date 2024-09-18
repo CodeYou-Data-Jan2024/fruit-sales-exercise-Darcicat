058f97360547806a7f2435e713b6c5a82dba7cc5
Darci <darcinevitt@gmail.com>
2024-09-18 18:49:18 -0400

    resolving typo

--- fruitsales.py
+++ fruitsales.py
@@ -1,5 +1,6 @@
 import pandas as pd
-fruit_sales = pd.DataFrame({"Apples" : ['35', '21'],
+fruit_sales = pd.DataFrame({"Apples" : ['35', '41'],
               "Bananas" : ['21', '34']},
-              index=['2017 Sales', '2018 Sales'])# add your code here
+              index=['2017 Sales', '2018 Sales'])
+print(fruit_sales)
 fruit_sales.to_csv('fruit.csv', index=True)
